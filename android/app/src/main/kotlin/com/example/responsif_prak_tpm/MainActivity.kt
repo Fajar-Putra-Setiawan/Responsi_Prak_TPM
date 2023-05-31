70218871ee40517777e0825b4268aff1c277680e
Fajar Putra Setiawan <fajar.putra1691@gmail.com>
2023-05-31 23:28:13 +0700

    first commit - Responsi Praktikum Tpm

---
+++ android/app/src/main/kotlin/com/example/responsif_prak_tpm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.responsif_prak_tpm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/category_model.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class Category {
+  List<Categories>? categories;
+
+  Category({this.categories});
+
+  Category.fromJson(Map<String, dynamic> json) {
+    if (json['categories'] != null) {
+      categories = <Categories>[];
+      json['categories'].forEach((v) {
+        categories!.add(new Categories.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (categories != null) {
+      data['categories'] = categories!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Categories {
+  String? idCategory;
+  String? strCategory;
+  String? strCategoryThumb;
+  String? strCategoryDescription;
+
+  Categories(
+      {this.idCategory,
+      this.strCategory,
+      this.strCategoryThumb,
+      this.strCategoryDescription});
+
+  Categories.fromJson(Map<String, dynamic> json) {
+    idCategory = json['idCategory'];
+    strCategory = json['strCategory'];
+    strCategoryThumb = json['strCategoryThumb'];
+    strCategoryDescription = json['strCategoryDescription'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['idCategory'] = idCategory;
+    data['strCategory'] = strCategory;
+    data['strCategoryThumb'] = strCategoryThumb;
+    data['strCategoryDescription'] = strCategoryDescription;
+    return data;
+  }
+}
